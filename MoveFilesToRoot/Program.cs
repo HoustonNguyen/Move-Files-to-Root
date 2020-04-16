7f270ee0b52fb789343671698432f6ee708c316a
HN <hue.dynamics@gmail.com>
2020-04-16 17:45:08 -0400

    Added initial files

---
+++ MoveFilesToRoot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MoveFilesToRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo baseDirectory = new DirectoryInfo(@"G:\TestBed");
+            List<FileInfo> files = baseDirectory.GetFiles("*.*", SearchOption.AllDirectories).ToList();
+
+            foreach(FileInfo file in files)
+            {
+                file.MoveTo(baseDirectory.FullName + "\\" + Guid.NewGuid().ToString() + file.Extension);
+            }
+        }
+    }
+}
